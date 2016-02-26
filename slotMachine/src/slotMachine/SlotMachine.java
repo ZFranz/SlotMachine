77e527e0d8ead34637868084fe4cadf6cddaa685
pellizzarigiacomo <giacomo.pellizzari4007@gmail.com>
2016-02-26 12:32:26 +0100

    Grafica

--- slotMachine/src/slotMachine/SlotMachine.java
+++ slotMachine/src/slotMachine/SlotMachine.java
@@ -2,10 +2,13 @@ package slotMachine;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
 
 public class SlotMachine {
 
-	protected Shell shell;
+	protected Shell shlZhoujackMachines;
 
 	/**
 	 * Launch the application.
@@ -26,9 +29,9 @@ public class SlotMachine {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlZhoujackMachines.open();
+		shlZhoujackMachines.layout();
+		while (!shlZhoujackMachines.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -39,10 +42,65 @@ public class SlotMachine {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlZhoujackMachines = new Shell();
+		shlZhoujackMachines.setSize(450, 400);
+		shlZhoujackMachines.setText("ZHOU&JACK MACHINES");
+		
+		Button btnReset = new Button(shlZhoujackMachines, SWT.NONE);
+		btnReset.setBounds(10, 302, 75, 50);
+		btnReset.setText("RESET");
+		
+		Button btnSpin = new Button(shlZhoujackMachines, SWT.NONE);
+		btnSpin.setBounds(349, 302, 75, 50);
+		btnSpin.setText("SPIN");
+		
+		Button btnBet = new Button(shlZhoujackMachines, SWT.NONE);
+		btnBet.setBounds(91, 302, 75, 50);
+		btnBet.setText("BET");
+		
+		Button btnBetAll = new Button(shlZhoujackMachines, SWT.NONE);
+		btnBetAll.setBounds(172, 302, 75, 50);
+		btnBetAll.setText("BET ALL");
+		
+		Label lblSaldo = new Label(shlZhoujackMachines, SWT.NONE);
+		lblSaldo.setBounds(10, 255, 55, 15);
+		lblSaldo.setText("SALDO");
+		
+		Label lblPuntata = new Label(shlZhoujackMachines, SWT.NONE);
+		lblPuntata.setBounds(192, 255, 55, 15);
+		lblPuntata.setText("PUNTATA");
+		
+		Label lblVincita = new Label(shlZhoujackMachines, SWT.NONE);
+		lblVincita.setBounds(349, 255, 55, 15);
+		lblVincita.setText("VINCITA");
+		
+		Label lblSaldonumero = new Label(shlZhoujackMachines, SWT.NONE);
+		lblSaldonumero.setBounds(10, 234, 55, 15);
+		lblSaldonumero.setText("0.0");
+		
+		Label lblPuntatanumero = new Label(shlZhoujackMachines, SWT.NONE);
+		lblPuntatanumero.setBounds(192, 234, 55, 15);
+		lblPuntatanumero.setText("0.0");
+		
+		Label lblVincitanumero = new Label(shlZhoujackMachines, SWT.NONE);
+		lblVincitanumero.setBounds(349, 234, 55, 15);
+		lblVincitanumero.setText("0.0");
+		
+		Label lblA = new Label(shlZhoujackMachines, SWT.NONE);
+		lblA.setBounds(30, 118, 55, 15);
+		lblA.setText("A");
+		
+		Label lblB = new Label(shlZhoujackMachines, SWT.NONE);
+		lblB.setBounds(192, 118, 55, 15);
+		lblB.setText("B");
+		
+		Label lblC = new Label(shlZhoujackMachines, SWT.NONE);
+		lblC.setBounds(349, 118, 55, 15);
+		lblC.setText("C");
+		
+		Label lblTitolo = new Label(shlZhoujackMachines, SWT.NONE);
+		lblTitolo.setBounds(192, 10, 55, 15);
+		lblTitolo.setText("TITOLO");
 
 	}
-
 }
