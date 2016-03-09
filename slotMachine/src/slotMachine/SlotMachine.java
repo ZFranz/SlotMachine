7c21026dc5414d2fd292836ff65aa36863442bc1
zhouzhiqiang <zhouzhiqiang@Lab06_01.itseinaudi.local>
2016-03-09 08:24:58 +0100

    img

--- slotMachine/src/slotMachine/SlotMachine.java
+++ slotMachine/src/slotMachine/SlotMachine.java
@@ -25,11 +25,11 @@ public class SlotMachine {
 
 	protected Shell shlZhoujackMachines;
 	ArrayList<String> immagini = new ArrayList<String>(){{
-		add("crome.jpg");
-		add("explorer.png");
-		add("nasa.png");
-		add("twitter.png");
-		add("xbox.png");
+		add("img/crome.jpg");
+		add("img/explorer.png");
+		add("img/nasa.png");
+		add("img/twitter.png");
+		add("img/xbox.png");
 	}};
 	private int j = 0;
 	private int k = 1;
@@ -100,25 +100,25 @@ public class SlotMachine {
 	protected void createContents() {
 		shlZhoujackMachines = new Shell();
 		shlZhoujackMachines.setBackground(SWTResourceManager.getColor(102, 51, 255));
-		shlZhoujackMachines.setImage(SWTResourceManager.getImage("icon.png"));
+		shlZhoujackMachines.setImage(SWTResourceManager.getImage("img/icon.png"));
 		shlZhoujackMachines.setSize(290, 405);
 		shlZhoujackMachines.setText("CHINA SLOTS");
-		shlZhoujackMachines.setBackgroundImage(SWTResourceManager.getImage("background.jpg"));
+		shlZhoujackMachines.setBackgroundImage(SWTResourceManager.getImage("img/background.jpg"));
 
 		Label lblA = new Label(shlZhoujackMachines, SWT.BORDER | SWT.WRAP);
-		lblA.setImage(SWTResourceManager.getImage("crome.jpg"));
+		lblA.setImage(SWTResourceManager.getImage("img/crome.jpg"));
 		lblA.setBounds(10, 88, 80, 80);
 
 		Label lblB = new Label(shlZhoujackMachines, SWT.BORDER | SWT.WRAP);
-		lblB.setImage(SWTResourceManager.getImage("explorer.png"));
+		lblB.setImage(SWTResourceManager.getImage("img/explorer.png"));
 		lblB.setBounds(96, 88, 80, 80);
 
 		Label lblC = new Label(shlZhoujackMachines, SWT.BORDER | SWT.WRAP);
-		lblC.setImage(SWTResourceManager.getImage("nasa.png"));
+		lblC.setImage(SWTResourceManager.getImage("img/nasa.png"));
 		lblC.setBounds(182, 88, 80, 80);
 
 		Label lblTitolo = new Label(shlZhoujackMachines, SWT.WRAP);
-		lblTitolo.setImage(SWTResourceManager.getImage("titolo.jpg"));
+		lblTitolo.setImage(SWTResourceManager.getImage("img/titolo.jpg"));
 		lblTitolo.setFont(SWTResourceManager.getFont("Gill Sans Ultra Bold Condensed", 17, SWT.NORMAL));
 		lblTitolo.setBounds(0, 0, 274, 82);
 
